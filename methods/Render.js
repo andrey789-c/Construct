e17e6ca88f0105ace9373d637c10e9e9473170a7
andrey789-c <andreymak2004@gmail.com>
2022-06-14 16:54:49 +1000

    Создание класса render

--- methods/Render.js
+++ methods/Render.js
@@ -1,5 +1,10 @@
 export class Render {
     constructor(elem, htmlContent) {
-        
+        this.elem = document.querySelector(elem)
+        this.htmlContent = htmlContent
+    }
+
+    render() {
+        this.elem.innerHTML = this.htmlContent
     }
 }
